420832fb71df83c5078f16a43b0aa25bff21c185
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2023-03-22 10:19:51 +0530

    Initial commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+const mission = "learn"//prompt("Enter what you are doing: ");
+
+if(mission === 'learn'){
+    console.log("Time to learn node js");
+} else {
+    console.log(`IS ${mission} really fun?`);
+}
